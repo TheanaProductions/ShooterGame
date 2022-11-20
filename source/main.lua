45f82a329570e7089c45b97d10809b136ab862e0
SinGala67 <galahane.rouille@gmail.com>
2022-11-20 09:49:42 +0100

    Initial commit

---
+++ source/main.lua
@@ -0,0 +1,11 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+function playdate.update()
+
+end
